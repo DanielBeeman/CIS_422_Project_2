cef7be6e619ddfd6c9ddcea33dc89ed72fa47802
Daniel Beeman <dlb@Daniels-MacBook-Pro-4.local>
2019-02-26 19:06:36 -0800

    added comments

--- Ticket_db.php
+++ Ticket_db.php
@@ -1,4 +1,10 @@
 <!DOCTYPE html>
+# Authors: Daniel Beeman and Jake Beder. Last Edited 2/26/19. This is the 
+# page that displays information about a ticket, or an error message if
+# there are no tickets to be displayed.  
+
+
+
 
 
 <?php
@@ -21,20 +27,24 @@ $conn = mysqli_connect($server, $user, $pass, $dbname, $port) or die('Error conn
 $name = $_POST['plate'];
 $name2 = $_POST['state'];
 
-
+# This code grabs information from the ticked database
 $query = "SELECT * FROM Parking_Ticket.Tickets AS Tic WHERE Tic.License_Plate LIKE ";
 
+# This searches specifically for the state specified by the user in the drop down menu
+# because there could be two cars with the same license plate, but in different states.
 $query = $query."'".$name."'";
 $query = $query." AND Tic.State LIKE ";
 $query = $query."'".$name2."';";
 
 
-
+# The result of the query
 $result = mysqli_query($conn, $query);
 
+# This keeps track of how many rows were returned, which helps us with the error message
 print "<pre>";
 $rowcount = mysqli_num_rows($result);
 
+# If there are no rows returned, we print the error page
 $check = 0;
 if ($rowcount > 0){
 	$check = 1;
@@ -43,7 +53,7 @@ if($check ==0){
 	echo "Error: No tickets have been found";
 }
 
-
+# This prints out information about tickets for a given license plate
 while ($event1 = mysqli_fetch_array($result, MYSQLI_BOTH)){
 		
 	print "Ticket number: $event1[idTickets], State: $event1[State], License Plate: $event1[License_Plate]";
@@ -51,6 +61,7 @@ while ($event1 = mysqli_fetch_array($result, MYSQLI_BOTH)){
 
 print"</pre>";
 
+# Free and close the sql connection
 mysqli_free_result($result);
 mysqli_close($conn);
 ?>
